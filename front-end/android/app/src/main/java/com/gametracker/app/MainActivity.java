dca519f4184e034a576dd8574f2255a03a4abeea
Ivax7 <xserrano2001@gmail.com>
2026-02-18 11:05:52 +0100

    Add Appflow config for frontend subforlder

---
+++ front-end/android/app/src/main/java/com/gametracker/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gametracker.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ front-end/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gametracker.app',
+  appName: 'GameTracker',
+  webDir: 'dist/front-end/browser'
+};
+
+export default config;
